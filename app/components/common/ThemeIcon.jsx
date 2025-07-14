e0ac365c9f40a8d23c208e0780b1d7585cad3c8f
Ronak <rroonnaakk7@gmail.com>
2025-07-14 16:36:32 +0530

    ref: removing extra imports and apply theme style

--- app/components/common/ThemeIcon.jsx
+++ app/components/common/ThemeIcon.jsx
@@ -1,6 +1,5 @@
 "use client";
 import { useTheme } from "next-themes";
-import { CiLight, CiDark } from "react-icons/ci";
 import { FaSun, FaMoon } from "react-icons/fa";
 
 const ThemeIcon = () => {
@@ -15,8 +14,8 @@ const ThemeIcon = () => {
 
   return (
     <div onClick={handleToggle} className="cursor-pointer relative w-4 h-4">
-      <FaSun className="absolute inset-0 hidden dark:block md:text-xl text-lg text-[#FFE28A]" />
-      <FaMoon className="absolute inset-0 block dark:hidden md:text-xl text-lg text-[#93C5FD]" />
+      <FaSun className="theme-icon hidden dark:block text-[#FFE28A]" />
+      <FaMoon className="theme-icon block dark:hidden text-[#93C5FD]" />
     </div>
   );
 };
